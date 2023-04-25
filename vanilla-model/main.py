62d919172998bd73881da50460440db6cde8e2a6
HuangBugWei <b06501042@g.ntu.edu.tw>
2023-04-25 16:28:42 +0800

    dummpy change

--- vanilla-model/main.py
+++ vanilla-model/main.py
@@ -2,23 +2,27 @@ import torch
 from model import voltageNN
 from dataset import voltageDataset
 
+
 model = voltageNN()
 dataloader = torch.utils.data.DataLoader(voltageDataset("/home/aicenter/cae_proj_raindrop/data/voltage"),
                                          shuffle = True,
                                          batch_size = 32,
                                          drop_last = True)
-epoch = 5000
+epoch = 50000
 
 loss_fn = torch.nn.MSELoss()
 optimizer = torch.optim.SGD(model.parameters(),
                              lr = 0.0001)
 
 for e in range(epoch):
+    totalLoss = []
     for idx, (x, y) in enumerate(dataloader):
         pred = model(x)
         
         loss = loss_fn(pred, y)
-        print(loss)
+        # print(loss)
+        totalLoss.append(loss.item())
         optimizer.zero_grad()
         loss.backward()
         optimizer.step()
+    print(sum(totalLoss)/len(totalLoss))
